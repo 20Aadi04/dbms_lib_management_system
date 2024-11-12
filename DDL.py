963df2bc946753939a85e22a54db3587f748c158
lokesh185 <lokeshkannan185@gmail.com>
2024-11-13 01:05:58 +0530

    changes

--- DDL.py
+++ DDL.py
@@ -10,70 +10,9 @@ print(database_url)
 conn = psycopg2.connect(database_url)
 
 with conn.cursor() as cur:
-    cur.execute("SELECT version()")
- 
-    # cur.execute("CREATE TYPE grad_type AS ENUM ('FIRST DEGREE', 'HIGHER DEGREE');")
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS Student (
-        Student_ID INT PRIMARY KEY,
-        FName VARCHAR(255) NOT NULL,
-        LName VARCHAR(255) NOT NULL,
-        DOB DATE NOT NULL,
-        Grad_Type grad_type NOT NULL,
-        Permitted_Book_count INT NOT NULL DEFAULT 3,
-        email VARCHAR(255) NOT NULL,
-        Phone_Num BIGINT NOT NULL UNIQUE
-    );
-    """)
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS Book (
-        B_ID SERIAL PRIMARY KEY,
-        B_Name VARCHAR(255) NOT NULL,
-        ISBN VARCHAR(255) NOT NULL,
-        Authors VARCHAR(255) [],
-        Pub VARCHAR(255) NOT NULL,
-        Category VARCHAR(255) NOT NULL
-    );
-    """)
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS Librarian (
-        Librarian_ID INT PRIMARY KEY,
-        FName VARCHAR(255) NOT NULL,
-        LName VARCHAR(255) NOT NULL,
-        DOB DATE NOT NULL,
-        Shift VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL
-    );
-    """)
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS Seat (
-        Seat_ID SERIAL PRIMARY KEY,
-        Location VARCHAR(255) NOT NULL,
-        Seat_No INT NOT NULL
-    );
-    """)
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS Booking (
-        Student_ID INT,
-        Seat_ID INT,
-        Start_Time TIMESTAMP NOT NULL,
-        End_Time TIMESTAMP NOT NULL,
-        Book_ids SERIAL [],
-        PRIMARY KEY(Student_ID,Start_Time,Seat_ID),
-        FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-        FOREIGN KEY (Seat_ID) REFERENCES Seat(Seat_ID)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-    );
-    """)
-
+    with open('DDL.sql','r') as file :
+        s = file.read()
+        cur.execute(s)
 
     cur.execute("SELECT * FROM STUDENT;")
     data = cur.fetchall()
