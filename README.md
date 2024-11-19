18d0aaa76ced6661180848a064fc375c643ab1fb
lokesh185 <lokeshkannan185@gmail.com>
2024-11-19 16:10:21 +0530

    add checks

--- README.md
+++ README.md
@@ -76,16 +76,28 @@ ORDER BY b.start_time DESC;
 4.  `@app.route('/current-booking', methods=['GET'])`
 
 ```sql
-SELECT
-s.seat_no, s.location, b.start_time, b.end_time,
-bo.b_name AS book_name, bo.isbn, bo.authors, bo.pub AS publisher
+SELECT s.seat_no,
+    s.location,
+    b.start_time,
+    b.end_time,
+    bo.b_name,
+    bo.isbn,
+    bo.authors,
+    bo.pub AS publisher
 FROM booking b
-JOIN seat s ON b.seat_id = s.seat_id
-JOIN bookingbook_id bb ON b.student_id = bb.student_id AND b.start_time = bb.start_time
-JOIN book bo ON bb.book_id = bo.b_id
-WHERE b.student_id = %s AND b.end_time > NOW()
-ORDER BY b.start_time
-LIMIT 1;
+    JOIN seat s ON b.seat_id = s.seat_id
+    LEFT JOIN bookingbook_id bb ON b.student_id = bb.student_id
+    AND b.start_time = bb.start_time
+    LEFT JOIN book bo ON bb.book_id = bo.b_id
+WHERE (b.student_id, b.start_time) in (
+        Select student_id,
+            start_time
+        from booking
+        where student_id = 20221011
+        ORDER by start_time
+        limit 1
+    )
+    AND b.end_time > NOW();
 ```
 
 ## function
--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,15 +1,19 @@
 -- CREATE TYPE IF NOT EXISTS grad_type AS ENUM ('FIRST DEGREE', 'HIGHER DEGREE');
-CREATE TABLE IF NOT EXISTS Student (
-    Student_ID INT PRIMARY KEY,
-    FName VARCHAR(255) NOT NULL,
-    LName VARCHAR(255) NOT NULL,
-    DOB DATE NOT NULL,
-    Grad_Type grad_type NOT NULL,
-    Permitted_Book_count INT NOT NULL DEFAULT 3,
-    email VARCHAR(255) NOT NULL,
-    Phone_Num BIGINT NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS student (
+    student_id INT PRIMARY KEY,
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    dob DATE NOT NULL,
+    grad_type grad_type NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE CHECK (
+        email ~ '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    ),
+    phone_num BIGINT NOT NULL UNIQUE CHECK (
+        phone_num >= 1000000000
+        AND phone_num <= 9999999999
+    ),
+    password VARCHAR(255) NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS Book (
     B_ID SERIAL PRIMARY KEY,
     B_Name VARCHAR(255) NOT NULL,
@@ -18,7 +22,6 @@ CREATE TABLE IF NOT EXISTS Book (
     Pub VARCHAR(255) NOT NULL,
     Category VARCHAR(255) NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS Librarian (
     Librarian_ID INT PRIMARY KEY,
     FName VARCHAR(255) NOT NULL,
@@ -27,13 +30,11 @@ CREATE TABLE IF NOT EXISTS Librarian (
     Shift VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS Seat (
     Seat_ID SERIAL PRIMARY KEY,
     Location VARCHAR(255) NOT NULL,
     Seat_No INT NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS Booking (
     Student_ID INT,
     Seat_ID INT,
@@ -44,7 +45,6 @@ CREATE TABLE IF NOT EXISTS Booking (
     FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (Seat_ID) REFERENCES Seat(Seat_ID) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 CREATE TABLE IF NOT EXISTS BookingBook_ID(
     Student_ID INT,
     Start_Time TIMESTAMP NOT NULL,
